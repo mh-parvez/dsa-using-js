511013ab313a800c28638028a68ba43908115fea
parvez469 <99886425+parvez469@users.noreply.github.com>
2024-01-31 23:36:08 +0600

    add asynchronous program

---
+++ Chapter-04-Asynchronus-Program/01_timeout_and_intervel.js
@@ -0,0 +1,31 @@
+// Todo: ⏰ SetTimeout:
+
+console.log("Script Start");
+
+const timeID = setTimeout(() => {
+  console.log("inside setTimeout");
+}, 1000);
+
+clearTimeout(timeID);
+
+console.log("Script End");
+
+// Todo: ⏰ SetIntervel:
+
+const body = document.body;
+const button = document.querySelector("button");
+
+const intervelID = setInterval(() => {
+
+  const red = Math.floor(Math.random() * 125);
+  const green = Math.floor(Math.random() * 125);
+  const blue = Math.floor(Math.random() * 125);
+
+  const rgb = `rgb(${red}, ${green}, ${blue})`;
+  body.style.background = rgb;
+
+}, 1000);
+
+button.addEventListener("click", () => {
+  clearInterval(intervelID);
+});
