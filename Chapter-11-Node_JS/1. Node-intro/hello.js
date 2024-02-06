c5010088be86b66727102fc2ab9933aa5a79330a
MH Parvez <99886425+mh-parvez@users.noreply.github.com>
2024-02-06 16:21:14 +0600

    add node js

---
+++ Chapter-11-Node_JS/1. Node-intro/hello.js
@@ -0,0 +1,10 @@
+
+console.log("Hello world")
+
+// console.log(window) // error
+// console.log(globalThis)
+
+// console.log("__filename : "+ __filename);
+// console.log("__dirname : "+ __dirname);
+ 
+
