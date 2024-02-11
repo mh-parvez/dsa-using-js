1a84c76dc5999923d0a22a8b9cdd9d8523ce2ab0
MH Parvez <99886425+mh-parvez@users.noreply.github.com>
2024-02-11 23:45:14 +0600

    Update 04_array.ts

--- Chapter-10-TypeScript/src/04_array.ts
+++ Chapter-10-TypeScript/src/04_array.ts
@@ -1,4 +1,5 @@
 const numbers: number[] = [4, 5, 5, 6, 6, 6];
+//const numbers:<number> = [4, 5, 5, 6, 6, 6];
 console.log(numbers);
 
 const modifedNumber: number[] = numbers.map((num: number) => num * num);
